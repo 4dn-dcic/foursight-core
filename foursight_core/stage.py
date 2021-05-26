cef1606bca02061860113e76600eac9fa8cc0aa4
William Ronchetti <wrr33@cornell.edu>
2021-05-26 10:59:14 -0400

    allow CHECK_RUNNER to be passed to give correct lambda name

--- foursight_core/stage.py
+++ foursight_core/stage.py
@@ -24,7 +24,10 @@ class Stage(object):
         return '-'.join([self.prefix, self.get_stage_from_env_variable(), 'check_queue'])
 
     def get_runner_name(self):
-        return '-'.join([self.prefix, self.get_stage(), 'check_runner'])
+        check_runner = os.environ.get('CHECK_RUNNER', None)
+        if not check_runner:
+            check_runner = '-'.join([self.prefix, self.get_stage(), 'check_runner'])
+        return check_runner
 
     @classmethod
     def is_stage_prod(cls):
