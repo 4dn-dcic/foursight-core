0f538b7f121327e3e73582e8cea3aef1656775d3
David Michaels <david_michaels@hms.harvard.edu>
2022-08-28 10:55:32 -0400

    React expriment dev.

---
+++ react/src/Envs.js
@@ -0,0 +1,13 @@
+import { LoginRequired } from "./LoginUtils.js";
+import * as URL from './URL';
+
+const Envs = (props) => {
+
+    return <div>
+        <h3>Environments:</h3>
+        <hr />
+        TODO
+    </div>
+};
+
+export default Envs;
---
+++ react/src/UnknownEnv.js
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import GlobalContext from "./GlobalContext.js";
+import { LoginRequired } from "./LoginUtils.js";
+import * as URL from './URL.js';
+
+const UnknownEnv = (props) => {
+
+    const [ info ] = useContext(GlobalContext);
+
+    return <LoginRequired>
+        <div className="container" id="login_container">
+            <div className="boxstyle check-warn" style={{margin:"20pt",padding:"10pt"}}>
+                <b>Unknown environment</b>.
+                <br />
+                <small>
+                Click <Link to={URL.Url("/view", true)} style={{color:"6F4E37"}}><b>here</b></Link> to go to return to the <Link to={URL.Url("/view", true)}><b style={{color:"6F4E37"}}>home</b></Link> page.
+                </small>
+            </div>
+        </div>
+    </LoginRequired>
+};
+
+export default UnknownEnv;
