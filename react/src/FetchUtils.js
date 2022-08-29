81764d8fc5bfabd27b53e7b3a4ec7bdf145335a5
David Michaels <david_michaels@hms.harvard.edu>
2022-08-29 14:55:02 -0400

    React expriment dev..

--- react/src/FetchUtils.js
+++ react/src/FetchUtils.js
@@ -6,6 +6,15 @@ function _sleep (time) {
 
 let _artificialSleepForTesting = GetCookie("test_mode_fetch_sleep");
 
+// The Foursight API we call (foursight-core/foursight_core/react_api.py) is protected
+// with a  new 'authtoken' cookie which was created along with the 'jwtToken' cookie;
+// We don't need to know this here but this authtokn is the jwtToken encrypted by the
+// API (server-side) using a password it got from its GAC (namely S3_AWS_ACCESS_KEY_ID).
+// We pass this authtoken cookie value as an 'authorization' header to the API.
+// The API decrypts this using its password (from the GAC) and checks that the
+// decrypted value looks like a valid JWT token and that it's not expired, etc.
+// I think this is reasonably secure.
+//
 export const fetchData = (url, setData, setLoading, setError) => {
     if (_artificialSleepForTesting > 0) {
         console.log("FETCHING WITH " + _artificialSleepForTesting + "ms SLEEP: " + url);
