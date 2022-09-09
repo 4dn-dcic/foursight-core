434d98bf45271fbd43ba92e00fee77d3ef12abcf
David Michaels <david_michaels@hms.harvard.edu>
2022-09-09 07:59:20 -0400

    React experiment.

--- react/src/Home.js
+++ react/src/Home.js
@@ -1,49 +1,24 @@
 import { useContext } from 'react';
-import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import GlobalContext from "./GlobalContext.js";
 import { LoginAndValidEnvRequired } from "./LoginUtils.js";
 import * as URL from "./URL";
-import ReactTooltip from 'react-tooltip';
-import { Tooltip } from './Tooltip';
 
 const Home = (props) => {
-    const [ tooltip, showTooltip ] = useState(true);
 
-        console.log("HOME")
-        console.log(window.location)
-        console.log(window.location.domain)
-        console.log(window.location.origin)
+    const [ header ] = useContext(GlobalContext);
+    let { environ } = useParams();
 
-    const [ info ] = useContext(GlobalContext);
-        let x = "a";
     return <LoginAndValidEnvRequired>
-                <ReactTooltip id="xyz">
-                adf
-                </ReactTooltip>
-                <p ref={ref => x = ref} data-for="xyz" data-tip="hello world" onClick={() => { ReactTooltip.hide("xyz"); console.log("Clik"); }}>Tooltip</p>
-                <b onClick={() => { ReactTooltip.hide(); console.log("clik"); }}>goaway</b>
-                <br/>
-                <br/>
-
-              <button data-tip data-for="registerTip" onClick={() => { ReactTooltip.hide("registerTip"); console.log("CLCKCCC"); }}>
-                Register
-              </button>
-                <br/>
-
-              { tooltip && <ReactTooltip id="registerTip" place="top" effect="solid"> Tooltip for the register button </ReactTooltip> }
-              <b onMouseEnter={() => showTooltip(true)} onMouseLeave={() => { showTooltip(false); setTimeout(() => showTooltip(true), 1); }}>adfdaf</b>
-                <br/>
-                <br/>
-                thisone
-        <div>
-                <Tooltip text="afdaf">
-                                        <img src="https://cdn-icons-png.flaticon.com/512/32/32223.png" style={{paddingBottom:"4px",height:"22"}} />
-                </Tooltip>
-        </div>
-
         <div className="container">
             <div className="boxstyle check-warn" style={{margin:"20pt",padding:"10pt",color:"#6F4E37"}}>
-                <b>Home Page</b>: TODO
+                <b>Home Page</b>
+                <p />
+                This is an experimental React version of Foursight. <br />
+                Click <b><a style={{color:"inherit"}} href={"/api/view/" + environ}>here</a></b> to go to the real version of Foursight. <br />
+                <small>
+                    For more info: david_michaels@hms.harvard.edu
+                </small>
             </div>
         </div>
     </LoginAndValidEnvRequired>
