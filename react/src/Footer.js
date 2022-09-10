3e49e5f740fbaf22d2a70439035f7960d04fac54
David Michaels <david_michaels@hms.harvard.edu>
2022-09-10 09:38:54 -0400

    React experiment.

--- react/src/Footer.js
+++ react/src/Footer.js
@@ -5,8 +5,21 @@ import GlobalContext from "./GlobalContext";
 const Footer = (props) => {
 
     const [ header ] = useContext(GlobalContext);
+    let isFoursightFourfront = header.app?.package != "foursight-cgap";
+    isFoursightFourfront =true
+
     return <>
-                TODO
+        <br />
+        <table width="100%"><tbody>
+            <tr style={{backgroundColor:"darkred",height:"1px"}}><td></td></tr>
+            <tr><td align="center" style={{paddingTop:"2px",paddingBottom:"6px"}}>
+                <a href={isFoursightFourfront ? ("https://" + header.env?.short_name + ".4dnucleome.org/") : "https://cgap.hms.harvard.edu/"} target="_blank">
+                
+                    <img src="https://www.iscb.org/images/stories/ismb2020/bazaar/logo.HarvardMedical-BiomedicalInformatics.png" height="46"/>
+                </a>
+            </td></tr>
+            <tr style={{backgroundColor:"darkred",height:"1px"}}><td></td></tr>
+        </tbody></table>
     </>
 };
 
