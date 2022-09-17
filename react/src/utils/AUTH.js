073cbe695a9414cf83f81b2b5ef066201e625851
David Michaels <david_michaels@hms.harvard.edu>
2022-09-17 10:30:38 -0400

    React experiment.

--- react/src/utils/AUTH.js
+++ react/src/utils/AUTH.js
@@ -31,12 +31,15 @@ function IsLoggedIn(header) {
     console.log("CHECK IS LOGGED IN");
     if (header?.auth?.authenticated) {
         console.log("IS LOGGED IN BY WAY OF HEADER DATA");
+        return true;
     }
     else if (COOKIE.HasAuthToken()) {
         console.log("IS LOGGED IN BY WAY OF COOKIE");
+        return true;
     }
     else {
         console.log("IS NOT LOGGED IN");
+        return false;
     }
     // return header?.auth?.authenticated || COOKIE.HasAuthToken();
 }
