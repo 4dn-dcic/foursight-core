128b6fef36656af73dd2ccb7aa90d49540661206
David Michaels <david_michaels@hms.harvard.edu>
2022-10-09 11:44:26 -0400

    React experiment.

--- react/src/ReadOnlyMode.js
+++ react/src/ReadOnlyMode.js
@@ -5,28 +5,13 @@ import Image from './utils/Image';
 const ReadOnlyModeGlobal = GlobalState.Define(() => Cookie.IsReadOnlyMode());
 const ReadOnlyModeDisplay = function() {
     const readOnlyMode = GlobalState.Use(ReadOnlyModeGlobal);
-    return <>
-        { readOnlyMode.value ? <>
-            <span className={"tool-tip"} data-text={"You are in readonly mode. Click to enter read/write mode."}>
-                <img alt="lock" src={Image.Lock()}
-                    style={{height:"30",cursor:"pointer"}}
-                    onClick={() => {
-                        readOnlyMode.update(false);
-                        Cookie.SetReadOnlyMode(false);
-                    }}/>
-        
-            </span>
-        </>:<>
-            <span className={"tool-tip"} data-text={"You are in read/write mode. Click to enter readonly mode."}>
-                <img alt="unlock" src={Image.Unlock()}
-                    style={{height:"30",cursor:"pointer"}}
-                    onClick={() => {
-                        readOnlyMode.update(true);
-                        Cookie.SetReadOnlyMode(true);
-                    }}/>
-            </span>
-        </>}
-    </>
+    return <span title={`You are in ${readOnlyMode.value ? 'readonly' : 'read/write'} mode. Click to enter ${readOnlyMode.value ? 'read/write' : 'readonly'} mode.`}>
+        <img alt="lock" src={readOnlyMode.value ? Image.Lock() : Image.Unlock()} style={{height:"35",cursor:"pointer"}}
+            onClick={() => {
+                Cookie.SetReadOnlyMode(!readOnlyMode.value);
+                readOnlyMode.update(!readOnlyMode.value);
+            }}/>
+    </span>
 }
 
 const exports = {
