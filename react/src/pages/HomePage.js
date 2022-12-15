066a5b042dc6d710a1b9a4b1f36425e71f85d2a9
David Michaels <david_michaels@hms.harvard.edu>
2022-12-15 17:44:16 -0500

    Minor UI tweak.

--- react/src/pages/HomePage.js
+++ react/src/pages/HomePage.js
@@ -35,12 +35,10 @@ const HomePage = (props) => {
                     <li> To view Foursight <b><Link to="/checks">checks</Link></b> click <b><Link to="/checks"><u>here</u></Link></b>.  </li>
                     <li> To view Foursight <b><Link to="/info">general</Link></b> info click <b><Link to="/info"><u>here</u></Link></b>.  </li>
                     <li> To view Foursight <b><Link to="/env">environments</Link></b> info click <b><Link to="/env"><u>here</u></Link></b>. </li>
-                    <li> To view Foursight <b><Link to="/network">network</Link></b> info click <b><Link to="/network"><u>here</u></Link></b>.  </li>
                     <li> To view Foursight <b><Link to="/users">users</Link></b> click <b><Link to="/users"><u>here</u></Link></b>.  </li>
                     <li> To view <b><Link to="/aws/s3">AWS S3</Link></b> info click <b><Link to="/aws/s3"><u>here</u></Link></b>.  </li>
-                    { (header.app?.accounts_file || header.app?.accounts_file_from_s3) &&
-                        <li> To view other <b><Link to="/accounts">accounts</Link></b> info click <b><Link to="/accounts"><u>here</u></Link></b>.  </li>
-                    }
+                    <li> To view <b><Link to="/network">AWS network</Link></b> info click <b><Link to="/network"><u>here</u></Link></b>.  </li>
+                    <li> To view other Foursight <b><Link to="/accounts">accounts</Link></b> click <b><Link to="/accounts"><u>here</u></Link></b>.  </li>
                 </ul>
             </div>
             <div className="box lighten thickborder" style={{margin:"20pt",padding:"10pt",marginTop:"-10pt"}}>
