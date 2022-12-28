21a429db29100e337ae3bf5c423518d4e9e1786c
David Michaels <david_michaels@hms.harvard.edu>
2022-12-28 18:57:33 -0500

    Starting experimental refactoring of some of checks components.

--- react/src/pages/checks/CheckBox.js
+++ react/src/pages/checks/CheckBox.js
@@ -1,6 +1,7 @@
 // REWRITE IN PROGRESS | NOT YET IN USE | 2022-12-27
 
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { StandardSpinner } from '../../Spinners';
@@ -10,6 +11,7 @@ import Char from '../../utils/Char';
 import Clipboard from '../../utils/Clipboard';
 import Env from '../../utils/Env';
 import Json from '../../utils/Json';
+import Str from '../../utils/Str';
 import Type from '../../utils/Type';
 import Yaml from '../../utils/Yaml';
 import Uuid from 'react-uuid';
@@ -19,14 +21,16 @@ import { useKeyedState, useOptionalKeyedState } from '../../Hooks.js';
 const background = "lightyellow";
 
 const TestCheckBox = (props) => {
+
+    let { environ } = useParams();
     const checkBoxState = useKeyedState();
     const [ show, setShow ] = useState(false);
-    // const checkName =  "biorxiv_is_now_published"; // "elastic_search_space";
-    const checkName =  "elastic_search_space";
+    //const checkName =  "elastic_search_space";
+    const checkName =  "biorxiv_is_now_published"; // "elastic_search_space";
     return <>
         { show ? <>
             <span className="pointer" onClick={() => setShow(value => !value)}>Hide CheckBox</span>
-            <CheckBox checkName={checkName} parentState={checkBoxState.keyed(checkName)} />
+            <CheckBox checkName={checkName} env={environ} parentState={checkBoxState.keyed(checkName)} />
         </>:<>
             <span className="pointer" onClick={() => setShow(value => !value)}>Show CheckBox</span>
         </>}
@@ -36,13 +40,9 @@ const TestCheckBox = (props) => {
 
 const CheckBox = (props) => {
 
-    let { checkName, environ, parentState } = props;
+    let { checkName, env, parentState } = props;
 
-    if (!checkName) {
-        checkName = "xyzzy_elastic_search_space";
-    }
-
-    const check = useFetch(`/checks/${checkName}`, { cache: true, delay: 1000 });
+    const check = useFetch(`/checks/${checkName}`, { cache: true, delay: 0 });
 
     const [ state, setState ] = useOptionalKeyedState(parentState);
     const isShowRunBox = () => state.showRunBox;
@@ -61,7 +61,7 @@ const CheckBox = (props) => {
             </span>
         </div>
         { isShowRunBox() && <>
-            <CheckRunBox check={check.data} env={environ} parentState={parentState.keyed("run-box")} />
+            <CheckRunBox check={check.data} env={env} parentState={parentState.keyed("run-box")} />
         </>}
     </div>
 }
@@ -76,14 +76,83 @@ const CheckRunBox = (props) => {
     function onActionRun(value) {
     }
 
+    // Parses out the the arguments for the check run from the info (ultimately) from the
+    // check_setup.json file and the check_function decorator. Returned object has a property
+    // named for each argument and its value an object with three propertie: "type" for its
+    // type, i.e. "boolean", "list", or "string"; "initial", its initial value; "list" for
+    // list type only containing the list of possible values; and "value" its current value,
+    // equal to "initial" but updated as the user updates with via UI.
+    //
     function getArgs(check, env) {
 
-            console.log('computing args ..................................................................')
-        if (!check || !check.schedule) {
-            return null;
+        // Extract the basic arguments (ultimately) from check_setup.json for the given check.
+        //
+        function extractArgs(check, env) {
+            if (check.schedule) {
+                for (let schedule_key of Object.keys(check.schedule)) {
+                    for (let schedule_env_key of Object.keys(check.schedule[schedule_key])) {
+                        if (schedule_env_key.toLowerCase() === "all" || Env.Equals(schedule_env_key, env)) {
+                            const args = {};
+                            const kwargs = check.schedule[schedule_key][schedule_env_key]["kwargs"];
+                            for (const kwargName in kwargs) {
+                                defineArg(args, kwargName, kwargs[kwargName]);
+                            }
+                            return args;
+                        }
+                    }
+                }
+            }
+            return {};
         }
 
-        function defineArg(name, value) {
+        // Amend the given extracted arguments with info (ultimately) from the check_function decorator
+        // for the tiven check. Adds any arguments not already present (ultimately) from check_setup.json
+        // via extractArgs above, and adds any initial values for those already present.
+        //
+        function amendArgs(args, check) {
+            if (check.registered_kwargs) {
+                //
+                // Factor in kwargs defined in the (check_function) decorator.
+                // Order here matters; have the kwargs (from check_setup.json) override the
+                // registered kwargs (from the check_function decorator); actually comes up for example for the wrangler_checks.core_project_status.
+                //
+                for (const kwargName in check.registered_kwargs) {
+                    if (args[kwargName]) {
+                        if (args[kwargName]?.type == "list") {
+                            //
+                            // Extract the initial value for a list type.
+                            //
+                            const kwargValue = check.registered_kwargs[kwargName];
+                            if (Type.IsArray(kwargValue)) {
+                                if (kwargValue.length > 0) {
+                                    //
+                                    // If multiple values for some reason in the check_function decorator,
+                                    // just pick the first one. I.e. we only support single selection lists.
+                                    //
+                                    args[kwargName].value = kwargValue[0];
+                                }
+                            }
+                            else if (Str.HasValue(kwargValue)) {
+                                //
+                                // To be generous allow either a list type (above) or
+                                // a string type for the initial value for a list type.
+                                //
+                                args[kwargName].value = kwargValue;
+                            }
+                        }
+                    }
+                    else {
+                        //
+                        // Argument not present (ultimately) in check_setup.json be
+                        // present in the check_function decorator.
+                        //
+                        defineArg(args, kwargName, check.registered_kwargs[kwargName]);
+                    }
+                }
+            }
+        }
+
+        function defineArg(args, name, value) {
             if (Type.IsBoolean(value)) {
                 args[name] = { type: "boolean", value: value };
             }
@@ -95,55 +164,16 @@ const CheckRunBox = (props) => {
             }
         }
 
-        let kwargs = null;
-        for (let schedule_key of Object.keys(check.schedule)) {
-            for (let schedule_env_key of Object.keys(check.schedule[schedule_key])) {
-                if (schedule_env_key.toLowerCase() === "all" || Env.Equals(schedule_env_key, env)) {
-                    kwargs = check.schedule[schedule_key][schedule_env_key]["kwargs"]
-                    break;
-                }
-            }
-            if (kwargs) {
-                break;
-            }
-        }
-
-        const args = {};
-
-        if (kwargs) {
-            for (const kwargName in  kwargs) {
-                defineArg(kwargName, kwargs[kwargName]);
-            }
-        }
-
-        if (check.registered_kwargs) {
-            //
-            // Factor in kwargs defined in the (check_function) decorator.
-            // Order here matters; have the kwargs (from check_setup.json) override the
-            // registered kwargs (from the check_function decorator); actually comes up for example for the wrangler_checks.core_project_status.
-            //
-            for (const registeredKwargName in check.registered_kwargs) {
-                if (args[registeredKwargName]) {
-                    if (args[registeredKwargName].type == "list") {
-                        const registeredKwargValue = check.registered_kwargs[registeredKwargName];
-                        if (Type.IsArray(registeredKwargValue)) {
-                            if (registeredKwargValue.length > 0) {
-                                args[registeredKwargName].value = registeredKwargValue[0];
-                            }
-                        }
-                        else if (Type.HasValue(registeredKwargValue)) {
-                            args[registeredKwargName].value = registeredKwargValue;
-                        }
-                    }
-                }
-                else {
-                    defineArg(registeredKwargName, check.registered_kwargs[registeredKwargName]);
-                }
-            }
-        }
+        const args = extractArgs(check, env);
+        amendArgs(args, check);
         return args;
     }
 
+    // Note we use the useOptionaKeyState hook passing in the passed in parentState
+    // which is from useKeyedState (TODO: need better names for these), so that
+    // our state gets stored in the parent, so that it can maintained between
+    // instantiations of this component, which can happen via component hide/show.
+    //
     const [ args, setArgs ] = useOptionalKeyedState(parentState, () => getArgs(check, env));
 
     return <div style={{width:"100%"}}>
@@ -222,7 +252,7 @@ const CheckRunArgList = (props) => {
                 const value = e.target.value;
                 setArg(value === EMPTY ? null : value);
             }}>
-            { !arg.initial && 
+            { (!arg.initial || !arg.value || arg.value == EMPTY) && 
                 <option key={0}>{EMPTY}</option>
             }
             { arg.list.map(item =>
