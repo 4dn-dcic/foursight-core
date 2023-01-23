23ba0fcb9ad631824c0da3ff13f6ab7e21f4f15f
David Michaels <david_michaels@hms.harvard.edu>
2023-01-23 18:10:40 -0500

    Fix for C4-972 where redirecting to confusing path with unknown login env.

--- react/src/Page.js
+++ react/src/Page.js
@@ -15,6 +15,9 @@ import useHeader from './hooks/Header';
 //
 function KnownEnvRequired({ children }) {
     const header = useHeader();
+    if (SanityCheckPath()) {
+        return;
+    }
     //
     // TODO: More fully understand this next line added 2022-09-16.
     // If not here then going to the /login page redirects to the /env page because /header API is still in progress.
@@ -41,6 +44,37 @@ function RedirectToDefaultEnvPath(header) {
     }
 }
 
+function SanityCheckPath() {
+    const path = window.location.pathname;
+    const components = path.substring(1).split("/");
+    if ((components.length >= 2) && ((components[0] !== "api") || (components[1] !== "react"))) {
+        //
+        // Catch specifically if we have a path without the required leading /api/react.
+        // This came up for example (C4-972) when we are on the Foursight Legacy site, e.g.
+        // at https://foursight.4dnucleome.org/view/webdev and then click on the link to go to
+        // Foursight React and it takes us to https://foursight.4dnucleome.org/api/react/login/env
+        // where the page complain about unknown environment "login" which is confusing; happens
+        // because the Foursight React link is https://foursight.4dnucleome.org/react/webdev/home
+        // i.e. lacking the required "/api"; could handle it at the link creation time but more
+        // robust just to catch it here more generally on the React side. And FYI note that it
+        // is unclear how that initial URL (https://foursight.4dnucleome.org/view/webdev) was
+        // navigated to in the first place; that should also have a "/api" prefix on the path.
+        //
+        if (components[0] === "react") {
+            window.location.pathname = `/api/${components.join("/")}`;
+            return true;
+        }
+        else {
+            if (components[0] === "api") {
+                components.shift();
+            }
+            window.location.pathname = `/api/react/${components.join("/")}`;
+            return true;
+        }
+    }
+    return false;
+}
+
 // If the user is NOT authenticated (i.e. logged in) OR is NOT authorized for the current
 // environment (i.e. if authenticated user is NOT allowed to access the environment from
 // the current URL, according to the allowed_envs list in the auth record of the global
@@ -49,7 +83,10 @@ function RedirectToDefaultEnvPath(header) {
 //
 function AuthorizationRequired({ children }) {
     const header = useHeader();
-    if (!Auth.IsLoggedIn(header)) {
+    if (SanityCheckPath()) {
+        return;
+    }
+    else if (!Auth.IsLoggedIn(header)) {
         return <Navigate to={Client.Path("/login")} replace />
     }
     else if (!Env.IsKnown(Env.Current(), header)) {
