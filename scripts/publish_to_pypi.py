a4c1b54d050743102d217bd4e25a4bc646f1286f
David Michaels <david_michaels@hms.harvard.edu>
2023-04-22 09:33:54 -0400

    debug

--- scripts/publish_to_pypi.py
+++ scripts/publish_to_pypi.py
@@ -49,9 +49,9 @@ def publish_package(pypi_username: str = None, pypi_password: str = None):
     verbose = True
     if verbose:
         print(" ".join(poetry_publish_command))
-    poetry_publish_results = execute_command(poetry_publish_command)
+    #poetry_publish_results = execute_command(poetry_publish_command)
     print('exec!/a')
-    #os.execv(poetry_command, poetry_publish_command)
+    os.execv(poetry_command, poetry_publish_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print('exec!/b')
     print('abc')
     print(poetry_publish_results)
