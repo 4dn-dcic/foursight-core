a81a473ac60be931c447f0384a9208abb9624cc5
David Michaels <david_michaels@hms.harvard.edu>
2023-06-16 16:33:33 -0400

    Working on fixing access key refresh to not run every day.

--- foursight_core/checks/access_key_expiration_detection.py
+++ foursight_core/checks/access_key_expiration_detection.py
@@ -35,7 +35,7 @@ def access_key_status(connection, **kwargs):
     one_week_to_expiration = expiration_date - timedelta(days=7)
     three_weeks_to_expiration = expiration_date - timedelta(days=21)
     now = datetime.now(most_recent_key_creation_date.tzinfo)
-    if now > one_week_to_expiration:
+    if True or (now > one_week_to_expiration):
         check.status = 'FAIL'
         check.summary = (f'Application access keys will expire in less than 7 days! Please run'
                          f' the deployment action ASAP')
