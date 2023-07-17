3c5659c7f85d942446888334f1422c70a2de46cf
David Michaels <david_michaels@hms.harvard.edu>
2023-07-16 23:16:03 -0400

    Adding support for local-check-runner utility.

--- foursight_core/scripts/local_check_runner.py
+++ foursight_core/scripts/local_check_runner.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from typing import Optional
 import yaml
 from foursight_core.captured_output import captured_output, uncaptured_output
 
@@ -32,6 +33,7 @@ def main():
     # lifecycle_checks/check_file_lifecycle_status
     # system_checks/elastic_search_space
 
+    print(args)
     run(args)
 
 
@@ -64,8 +66,8 @@ def run(args):
         check_info = handler.get_check_info(args.check_or_action)
         if not check_info:
             raise Exception(f"Check not found: {args.check_or_action}")
-        check_args = collect_args(check_info, verbose=args.verbose)
-        check_args["primary"] = args.primary
+        check_args = {"primary": True} if args.primary else None
+        check_args = collect_args(check_info, initial_args=check_args, verbose=args.verbose)
         confirm_interactively(f"Run check {args.check_or_action}?", exit_if_no=True)
         check_result = handler.run_check_or_action(connection, args.check_or_action, check_args)
         PRINT(json.dumps(check_result, indent=4))
@@ -78,29 +80,28 @@ def run(args):
                 action_info = handler.get_action_info(check_info.associated_action)
                 if not action_info:
                     raise Exception(f"Action not found: {check_info.associated_action}")
-                action_args = collect_args(check_info, verbose=args.verbose)
-                action_args["check_name"] = check_info.name
-                action_args["called_by"] = uuid
+                action_args = {"check_name": check_info.name, "called_by": uuid}
+                action_args = collect_args(check_info, initial_args=action_args, verbose=args.verbose)
                 action_result = handler.run_check_or_action(connection, action_info.qualified_name, action_args)
                 PRINT(json.dumps(action_result, indent=4))
 
 
-def collect_args(check_or_action_info, verbose: bool = False) -> dict:
-    args = {}
-    if check_or_action_info.kwargs:
-        kind = check_or_action_info.kind
-        name = check_or_action_info.qualified_name
-        with uncaptured_output():
+def collect_args(check_or_action_info, initial_args: Optional[dict] = None, verbose: bool = False) -> dict:
+    args = initial_args or {}
+    kind = check_or_action_info.kind
+    name = check_or_action_info.qualified_name
+    with uncaptured_output():
+        if check_or_action_info.kwargs:
             print(f"Prompting for {kind} arguments for {name}:")
             for arg_name in check_or_action_info.kwargs:
                 arg_default = check_or_action_info.kwargs[arg_name]
                 value = input(f"Enter {kind} argument => "
                               f"{arg_name} [default: {arg_default}]: ")
                 args[arg_name] = value
-            if verbose:
-                print(f"Specified check arguments for {name}:")
-                for check_arg in args:
-                    print(f"=> {check_arg}: {args[check_arg]}")
+        if verbose:
+            print(f"Check arguments for {name}:")
+            for check_arg in args:
+                print(f"=> {check_arg}: {args[check_arg]}")
     return args
 
 
