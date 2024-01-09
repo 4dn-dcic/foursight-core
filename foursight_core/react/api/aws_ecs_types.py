5eefd71bac4bf4a5e6c718628a10782ec12ef66f
David Michaels <david_michaels@hms.harvard.edu>
2024-01-08 22:59:28 -0500

    typo

--- foursight_core/react/api/aws_ecs_types.py
+++ foursight_core/react/api/aws_ecs_types.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 KNOWN_TASK_DEFINITION_OR_SERVICE_MAP = {re.compile(key, re.IGNORECASE): value for key, value in {
-    r"(.*deploy.*initial.*)|(.*deploy.*initial.*)": "deploy_initial",
+    r"(.*deploy.*initial.*)|(.*initial.*deploy.*)": "deploy_initial",
     r"^(?!.*initial).*deploy.*$": "deploy",  # matches string containing 'deploy' but not 'initial'.
     r".*index.*": "indexer",
     r".*ingest.*": "ingester",
